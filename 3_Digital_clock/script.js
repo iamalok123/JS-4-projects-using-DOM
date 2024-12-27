667fbec9788683949ec95792008857ceca218ade
Alok Hotta <155811131+iamalok123@users.noreply.github.com>
2024-12-27 18:41:10 +0530

    Add files via upload

---
+++ 3_Digital_clock/script.js
@@ -0,0 +1,21 @@
+const clock = document.getElementById('clock');
+
+setInterval(function () {
+  let date = new Date();
+  console.log(date.toLocaleTimeString());
+  clock.innerHTML = date.toLocaleTimeString();
+}, 1000);
+
+
+
+// or 
+
+
+
+// const clock = document.querySelector('#clock');
+
+// setInterval(function () {
+//   let date = new Date();
+//   console.log(date.toLocaleTimeString());
+//   clock.innerHTML = date.toLocaleTimeString();
+// }, 1000);
